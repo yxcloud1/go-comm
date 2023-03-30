2155995c9fcb04970b1d3f11104c1fc356006666
yyf <yxcloud@139.com>
2023-03-30 08:29:02 +0800

    add winservice

---
+++ winservice/winservice.go
@@ -0,0 +1,107 @@
+package winservice
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-comm/logger"
+	"github.com/kardianos/service"
+)
+
+type ServiceProgram interface {
+	Run() error
+	Kill() error
+}
+
+type WinService struct {
+	prog   ServiceProgram
+	config *service.Config
+}
+
+type ServiceInstance struct{
+	kill func() error
+	run func() error
+}
+
+func (s* ServiceInstance) Run() error{
+	if s.run != nil{
+		return s.run()
+	}
+	return nil
+}
+
+func (s* ServiceInstance) Kill() error{
+	if s.kill != nil{
+		return s.kill()
+	}
+	return nil
+}
+
+
+func (p *WinService) Start(s service.Service) error {
+	go p.prog.Run()
+	logger.TxtLog("服务已经启动")
+	return nil
+}
+
+func (p *WinService) Stop(s service.Service) error {
+	err := p.prog.Kill()
+	if err != nil {
+		logger.TxtErr(p.config.Name, err)
+	}
+	logger.TxtLog("服务已经停止")
+	return err
+}
+
+
+
+func RunAsService(serviceName string, displayName string, description string, run func() error,  kill func() error) {
+
+	svrCfg := &service.Config{
+		Name:        serviceName,
+		DisplayName: displayName,
+		Description: description,
+	}
+
+	//	logFile, _ := os.OpenFile(filepath.Dir(os.Args[0])+"/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	svr := &WinService{
+		config: svrCfg,
+		prog:   &ServiceInstance{
+			kill: kill,
+			run: run,
+		},
+	}
+	s, err := service.New(svr, svrCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*	log.SetOutput(logFile)
+
+
+	 */
+	if len(os.Args) > 1 {
+		if os.Args[1] == "install" {
+			err := s.Install()
+			if err == nil {
+				logger.TxtLog(serviceName, "服务安装成功！")
+			} else {
+				logger.TxtErr(serviceName, err)
+			}
+			return
+		}
+		if (os.Args[1]) == "remove" {
+			err = s.Uninstall()
+			if err == nil {
+				logger.TxtLog(serviceName, "服务卸载成功！")
+			} else {
+				logger.TxtErr(serviceName, err)
+			}
+			return
+		}
+	}
+	err = s.Run()
+	if err != nil {
+		logger.TxtErr(serviceName, serviceName, err)
+	}
+
+}
